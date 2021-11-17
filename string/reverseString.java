6c1ed19d1378cef8af3d2d0642d6e04c4b0dfd11
Harsimran singh <simran.9971290436@gmail.com>
2021-11-17 15:59:51 +0530

    reverse a string

---
+++ string/reverseString.java
@@ -0,0 +1,17 @@
+String reverseWords(String S)
+    {
+        // code here 
+        String str = "";
+ 
+       
+        String[] arr = S.split("\\.");
+        for(int i=arr.length-1;i>=0;i--){
+            str += arr[i];
+           
+            if(i>0){
+                str += ".";
+            }
+            
+        }
+        return str;
+    }
